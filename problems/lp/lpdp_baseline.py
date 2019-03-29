b9e0896120acdddc031025b02bfe7fec3c25b507
Sergey Ivanov <ivanovserg990@criteo.com>
2019-03-29 18:53:40 +0100

    Update the path

--- problems/lp/lpdp_baseline.py
+++ problems/lp/lpdp_baseline.py
@@ -2,7 +2,7 @@ from subprocess import check_call, check_output
 import argparse
 
 def run_kalp(fn, start, end):
-    out = check_output(['deploy/kalp', fn, '--start_vertex', start,
+    out = check_output(['KaLPv2.0/deploy/kalp', fn, '--start_vertex', start,
                         '--target_vertex', end])
     print(out)
 
@@ -10,8 +10,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", help="Filename to run the algorithm")
-    parser.add_argument("-s", type=int, help="Start vertex")
-    parser.add_argument("-t", type=int, help="End vertex")
+    parser.add_argument("-s", type=str, help="Start vertex")
+    parser.add_argument("-t", type=str, help="End vertex")
 
     opts = parser.parse_args()
 
